ce49841bc8f791d78d5f5206430d1b1cfa9b6f84
mame0622 <mame.like.ww@gmail.com>
2023-09-04 11:37:39 +0900

    プログラム初期設定

--- Source/Mame/Scene/SceneGame.h
+++ Source/Mame/Scene/SceneGame.h
@@ -1,6 +1,12 @@
 #pragma once
+
 #include "BaseScene.h"
 
+#include "../Graphics/SkyBox.h"
+#include "../Graphics/ShadowMap.h"
+#include "../Resource/sprite.h"
+
+#define SKY_BOX 1
 
 
 class SceneGame : public Mame::Scene::BaseScene
@@ -18,5 +24,30 @@ public:
     void Render(const float& elapsedTime) override; // ï`âÊèàóù
 
     void DrawDebug()    override;
+
+private:
+    bool isDebugCamera = false;
+
+private:
+#if SKY_BOX
+    std::shared_ptr<Sprite> skyBoxSprite;
+    std::unique_ptr<SkyBox> skyBox;
+#endif// SKY_BOX
+
+    // SHADOW
+    Microsoft::WRL::ComPtr<ID3D11Buffer> SceneConstantBuffer;
+
+    // SHADOW
+    struct Shadow
+    {
+        const uint32_t shadowMapWidth = 2048;
+        const uint32_t shadowMapHeight = 2048;
+        std::unique_ptr<ShadowMap> shadowMap;
+        DirectX::XMFLOAT4 lightViewFocus{ 0,0,0,1 };
+        float lightViewDistance = 10.0f;
+        float lightViewSize = 12.0f;
+        float lightViewNearZ = 2.0f;
+        float lightViewFarZ = 18.0f;
+    }shadow;
 };
 
